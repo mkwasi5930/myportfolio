7a13d6c23e0106c26b22ab31d63713592d7c7b76
mkwasi5930 <abedtati1@gmail.com>
2023-02-02 13:31:39 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="header">
+	<h2>Home Page</h2>
+</div>
+<div class="content">
+  	
+     
+    	<p>Thank you very much for choosing AbedTati.</br>
+	Am here to serve you.</p>
+    	
+</div>
+
+
+		
+</body>
+</html>
